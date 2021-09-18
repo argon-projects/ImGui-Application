51d3d93597f6cf5adc4a6cfd41d40f5a3a61530b
Argon Projects <89998024+argon-projects@users.noreply.github.com>
2021-09-19 00:17:26 +0200

    Update entry.cpp

--- DirectX-Application/entry.cpp
+++ DirectX-Application/entry.cpp
@@ -1,11 +1,6 @@
 #include "pch.hpp"
 #include "entry.hpp"
 
-#define MAX_LOADSTRING 100
-
-
-#include "c_directx.hpp"
-
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
 {
     UNREFERENCED_PARAMETER(hPrevInstance);
